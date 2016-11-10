add3d2a9488f1a69f1b31acd35939cbcd1bb0e9d
Yoshi <thangnv77@gmail.com>
2016-11-10 11:18:34 +0700

    Remove source-map-support in library

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import sourceMapSupport = require('source-map-support');
-sourceMapSupport.install();
-
 export { loadRoutes } from './core';
 export { register } from './hapiIntegration';
 export * from './types';
