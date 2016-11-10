b25c786a9af3c325c4be2d3afba1c87c5d11420b
Yoshi <thangnv77@gmail.com>
2016-11-10 11:07:40 +0700

    Add source-map-support

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import sourceMapSupport = require('source-map-support');
+sourceMapSupport.install();
+
 export { loadRoutes } from './core';
 export { register } from './hapiIntegration';
 export * from './types';
